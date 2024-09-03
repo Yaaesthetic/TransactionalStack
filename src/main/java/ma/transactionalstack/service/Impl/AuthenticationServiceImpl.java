7d1777b1800c4f0273c7c7a9953a88bf6eb62a97
Yassine_K <kacimiyassine16@gmail.com>
2024-09-03 01:23:20 +0100

    first

--- src/main/java/ma/transactionalstack/service/Impl/AuthenticationServiceImpl.java
+++ src/main/java/ma/transactionalstack/service/Impl/AuthenticationServiceImpl.java
@@ -6,7 +6,6 @@ import ma.transactionalstack.repository.AuthedRepo;
 import ma.transactionalstack.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.IllegalTransactionStateException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
